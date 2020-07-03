daabf4c337dea663054d4b9d5b4490434ba0e59e
samedamci <samedamci@disroot.org>
2020-07-03 10:25:54 +0200

    Add inline mode

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
-from telegram import ParseMode
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Dispatcher
+from telegram import ParseMode, InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import Updater, CommandHandler, InlineQueryHandler
 from telegram.error import BadRequest, TimedOut
 import logging
 import requests
 import json
+from random import randint
 from settings import TOKEN, INSTANCE_URL
 
 logging.basicConfig(format="%(levelname)s - %(message)s", level=logging.DEBUG)
@@ -24,6 +25,13 @@ def start(update, context):
     )
 
 
+def request_(query):
+    return requests.post(
+        INSTANCE_URL,
+        data={"q": " ".join(query), "language": "en-US", "format": "json"},
+    )
+
+
 def searx(update, context):
     try:
         if int(context.args[0]) > 10:
@@ -34,20 +42,17 @@ def searx(update, context):
     except ValueError:
         results_number = 1
 
-    response = requests.post(
-        INSTANCE_URL,
-        data={"q": " ".join(context.args), "language": "en-US", "format": "json"},
-    )
+    response = request_(context.args)
 
     for i in range(results_number):
         try:
             result = json.loads(response.text)["results"][i]
             query_link = (
-                response.url + "?" + response.request.body.replace('&format=json', '')
+                response.url + "?" + response.request.body.replace("&format=json", "")
             )
 
             try:
-                content = f"\n{result['content']}\n"
+                content = result["content"]
             except KeyError:
                 content = ""
 
@@ -55,8 +60,8 @@ def searx(update, context):
                 update.message.reply_text(
                     f"""
 *{result['title']}*
-[query link]({query_link}) | [direct link]({result['pretty_url']})
-{content}
+[direct link]({result['pretty_url']}) | [query link]({query_link})
+\n{content}\n
 `from: {", ".join(result['engines'])}`""",
                     parse_mode=ParseMode.MARKDOWN,
                 )
@@ -71,6 +76,45 @@ def searx(update, context):
             update.message.reply_text("No results.")
 
 
+def inline(update, context):
+    answers = []
+    query = list(str(update.inline_query.query).split(" "))
+    if not query:
+        return
+    response = request_(query)
+    for i in range(0, 10):
+        result = json.loads(response.text)["results"][i]
+        title = result["title"]
+        query_link = (
+            response.url + "?" + response.request.body.replace("&format=json", "")
+        )
+        try:
+            content = result["content"]
+        except KeyError:
+            content = ""
+        try:
+            answers.append(
+                InlineQueryResultArticle(
+                    id=randint(0, 10000),
+                    title=title[:32] + (title[29:] and "..."),
+                    description=content[:32] + (content[29:] and "..."),
+                    input_message_content=InputTextMessageContent(
+                        f"""
+*{title}*
+[direct link]({result['pretty_url']}) | [query link]({query_link})
+\n{content}\n
+`from: {", ".join(result['engines'])}`""",
+                        parse_mode=ParseMode.MARKDOWN,
+                    ),
+                )
+            )
+        except IndexError:
+            answers.append(
+                InlineQueryResultArticle(id="no results", title="no results")
+            )
+    context.bot.answer_inline_query(update.inline_query.id, answers)
+
+
 def main():
     global updater
     updater = Updater(TOKEN, use_context=True)
@@ -78,6 +122,7 @@ def main():
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", start))
     dispatcher.add_handler(CommandHandler("searx", searx))
+    dispatcher.add_handler(InlineQueryHandler(inline))
     updater.start_polling()
     updater.idle()
 
